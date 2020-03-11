b7684acfbd6eb8081c28dc422a76b219c9ba64a4
Emma Troxell <troxellemma@gmail.com>
2020-03-10 22:04:22 -0500

    implemented Checkbox constructor + 4 overloads

--- QuizTime/Checkbox.cs
+++ QuizTime/Checkbox.cs
@@ -4,7 +4,68 @@ using System.Text;
 
 namespace QuizTime
 {
-    class Checkbox
+    class Checkbox : Question
     {
+        internal List<string> AnswerChoices { get; set; }
+        internal List<string> CorrectAnswers { get; set; }
+        internal Checkbox(string questionToAsk, string answer1, string answer2, string answer3, string answer4, string correctAnswer1) : base(questionToAsk)
+        {
+            List<string> answerChoices = new List<string>();
+            List<string> correctAnswers = new List<string>();
+            answerChoices.Add(answer1);
+            answerChoices.Add(answer2);
+            answerChoices.Add(answer3);
+            answerChoices.Add(answer4);
+            correctAnswers.Add(correctAnswer1);
+            AnswerChoices = answerChoices;
+            CorrectAnswers = correctAnswers;
+           
+        }
+        internal Checkbox(string questionToAsk, string answer1, string answer2, string answer3, string answer4, string correctAnswer1, string correctAnswer2) : base(questionToAsk)
+        {
+            List<string> answerChoices = new List<string>();
+            List<string> correctAnswers = new List<string>();
+            answerChoices.Add(answer1);
+            answerChoices.Add(answer2);
+            answerChoices.Add(answer3);
+            answerChoices.Add(answer4);
+            correctAnswers.Add(correctAnswer1);
+            correctAnswers.Add(correctAnswer2);
+            AnswerChoices = answerChoices;
+            CorrectAnswers = correctAnswers;
+
+        }
+        internal Checkbox(string questionToAsk, string answer1, string answer2, string answer3, string answer4, string correctAnswer1, string correctAnswer2, string correctAnswer3) : base(questionToAsk)
+        {
+            List<string> answerChoices = new List<string>();
+            List<string> correctAnswers = new List<string>();
+            answerChoices.Add(answer1);
+            answerChoices.Add(answer2);
+            answerChoices.Add(answer3);
+            answerChoices.Add(answer4);
+            correctAnswers.Add(correctAnswer1);
+            correctAnswers.Add(correctAnswer2);
+            correctAnswers.Add(correctAnswer3);
+            AnswerChoices = answerChoices;
+            CorrectAnswers = correctAnswers;
+
+        }
+        internal Checkbox(string questionToAsk, string answer1, string answer2, string answer3, string answer4, string correctAnswer1, string correctAnswer2, string correctAnswer3, string correctAnswer4) : base(questionToAsk)
+        {
+            List<string> answerChoices = new List<string>();
+            List<string> correctAnswers = new List<string>();
+            answerChoices.Add(answer1);
+            answerChoices.Add(answer2);
+            answerChoices.Add(answer3);
+            answerChoices.Add(answer4);
+            correctAnswers.Add(correctAnswer1);
+            correctAnswers.Add(correctAnswer2);
+            correctAnswers.Add(correctAnswer3);
+            correctAnswers.Add(correctAnswer4);
+            AnswerChoices = answerChoices;
+            CorrectAnswers = correctAnswers;
+
+        }
+
     }
 }
